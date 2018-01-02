2103358c6e2b8097838058b2fcb943505b8a8b67
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-01-01 23:46:16 -0700

    Becoming limitless.

---
+++ mappingFunctions.js
@@ -0,0 +1,26 @@
+/**
+ * Mapping functions will control how the data is represented on the map
+ *
+ * We will change the name upon completion to avoid naming collisions
+ *
+ * USES MapQuest.js 1.3
+ *
+ */
+
+
+/**
+ * Notes:
+ * A parking marker has
+ * Fixed Point Coords
+ * StationID - 6 digit (this should acutally be the fixedpoint GPS
+ * Last Reading - timestamp of last data post
+ * 
+ */
+
+
+
+function PlotMarkers(){
+
+
+}
+
